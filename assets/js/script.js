80e1caa4e20b1e4108c7f5a65081f16ddadaba70
yash <yporwal333@gmail.com>
2024-07-06 18:13:32 +0530

    testimonial and blog section created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(function(){
     // alert("Hello");
+    $('.testimonial-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows:true,
+        centerMode: false,
+      });
 });
